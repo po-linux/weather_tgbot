331a32e06b605014e722a4123704b80e0f11fb82
Polina <kaplenkova2014@gmail.com>
2022-12-02 19:34:38 +0300

    parsing weather api

--- tgbot/handlers/weather.py
+++ tgbot/handlers/weather.py
@@ -1,19 +1,36 @@
 from aiogram import types, Dispatcher
-import json
 import requests
+from environs import Env
 
-api_key = 'f95e776f6f05ffd238e6709cd03db248'
-lat, lon = 55.75, 37.62
-api_path = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric"
+env = Env()
+env.read_env(".env")
+
+
+def get_coords():
+    lat, lon = 55.75, 37.62
+    return lat, lon
+
+
+def get_api_path():
+    lat, lon = get_coords()
+    api_key = env.str("WT_API_KEY")
+    api_host = env.str("WT_API_HOST")
+    api_params = f"units=metric&lat={lat}&lon={lon}&appid={api_key}"
+    api_path = api_host + "?" + api_params
+    return api_path
 
 
 async def get_weather(message: types.Message):
+    api_path = get_api_path()
     response = requests.get(api_path)
-    wt_dict = response.json()
-    wt_json = json.dumps(wt_dict)
-    await message.answer(wt_json)
+    wt_json = response.json()
+    city = wt_json["name"]
+    temp = wt_json["main"]["temp"]
+    feels = wt_json["main"]["feels_like"]
+    msg = f"Температура в {city} равна {temp}°C.\n"
+    msg += f"Ощущается как {feels}°C."
+    await message.answer(msg)
 
 
 def register_weather(dp: Dispatcher):
     dp.register_message_handler(get_weather, commands=["weather"], state="*")
-
