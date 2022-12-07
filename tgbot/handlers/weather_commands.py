73b7834e88dd7be313b266ba79f2d82d3dbfff73
Полина <polina@iMac.local>
2022-12-07 23:18:31 +0300

    Добавила разные клавиатуры для разных комманд

--- tgbot/handlers/weather_commands.py
+++ tgbot/handlers/weather_commands.py
@@ -1,41 +1,47 @@
 from aiogram import types, Dispatcher
 
 import tgbot.handlers.weather as wt
+import tgbot.handlers.coordinates as crd
 from tgbot.keyboards import inline
 
-
-location = "Москва"
+#location = "Москва"
 
 # Commands
 async def cmd_temperature(message: types.Message):
+    location = crd.get_location()
     msg = wt.get_temperature(location)
-    await message.answer(msg, reply_markup=inline.WEATHER)
+    await message.answer(msg, reply_markup=inline.TEMPERATURE)
 
 
 async def cmd_wind(message: types.Message):
+    location = crd.get_location()
     msg = wt.get_wind(location)
-    await message.answer(msg, reply_markup=inline.WEATHER)
+    await message.answer(msg, reply_markup=inline.WIND)
 
 
 async def cmd_suntime(message: types.Message):
+    location = crd.get_location()
     msg = wt.get_suntime(location)
-    await message.answer(msg, reply_markup=inline.WEATHER)
+    await message.answer(msg, reply_markup=inline.SUNTIME)
 
 
 # Callbaks
 async def call_temperature(callback: types.CallbackQuery):
+    location = crd.get_location()
     msg = wt.get_temperature(location)
-    await callback.message.answer(msg, reply_markup=inline.WEATHER)
+    await callback.message.answer(msg, reply_markup=inline.TEMPERATURE)
 
 
 async def call_wind(callback: types.CallbackQuery):
+    location = crd.get_location()
     msg = wt.get_wind(location)
-    await callback.message.answer(msg, reply_markup=inline.WEATHER)
+    await callback.message.answer(msg, reply_markup=inline.WIND)
 
 
 async def call_suntime(callback: types.CallbackQuery):
+    location = crd.get_location()
     msg = wt.get_suntime(location)
-    await callback.message.answer(msg, reply_markup=inline.WEATHER)
+    await callback.message.answer(msg, reply_markup=inline.SUNTIME)
 
 
 def register_weather(dp: Dispatcher):
--- tgbot/keyboards/inline.py
+++ tgbot/keyboards/inline.py
@@ -5,8 +5,8 @@ BTN_TEMPERATURE = InlineKeyboardButton('Temperature', callback_data='temperature
 BTN_WIND = InlineKeyboardButton('Wind', callback_data='wind')
 BTN_SUNTIME = InlineKeyboardButton('Suntime', callback_data='suntime')
 
-WEATHER = InlineKeyboardMarkup().add(BTN_TEMPERATURE, BTN_WIND, BTN_SUNTIME)
-# TEMPERATURE = InlineKeyboardMarkup().add(BTN_WIND, BTN_SUNTIME)
-# IND = InlineKeyboardMarkup().add(BTN_TEMPERATURE).add(BTN_SUNTIME)
-# SUN_TIME = InlineKeyboardMarkup().add(BTN_TEMPERATURE, BTN_WIND)
+#WEATHER = InlineKeyboardMarkup().add(BTN_TEMPERATURE, BTN_WIND, BTN_SUNTIME)
+TEMPERATURE = InlineKeyboardMarkup().add(BTN_WIND, BTN_SUNTIME)
+WIND = InlineKeyboardMarkup().add(BTN_TEMPERATURE).add(BTN_SUNTIME)
+SUNTIME = InlineKeyboardMarkup().add(BTN_TEMPERATURE, BTN_WIND)
 # HELP = InlineKeyboardMarkup().add(BTN_TEMPERATURE, BTN_WIND).add(BTN_SUNTIME)
