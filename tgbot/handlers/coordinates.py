bb9eda213f141610c4028ffb8070d99f984b3630
Polina <kaplenkova2014@gmail.com>
2022-12-15 11:20:51 +0300

    added FSM

--- tgbot/handlers/coordinates.py
+++ tgbot/handlers/coordinates.py
@@ -4,11 +4,12 @@ from environs import Env
 
 from aiogram import types, Dispatcher
 from tgbot.keyboards import inline
+from tgbot.states.location import Location
+from aiogram.dispatcher.storage import FSMContext
+
 
 env = Env()
 env.read_env(".env")
-
-location = "Москва"
 
 
 def get_crd_api_url(location):
@@ -30,21 +31,38 @@ def get_coordinates(location):
     except:
         coordinates = False
     return coordinates
-
-
-# эта функция нужна, чтобы из других модулей получить значение location
+'''
+ эта функция нужна, чтобы из других модулей получить значение location
 def get_location():
     global location
     return location
+'''
+
+
+async def get_location(state: FSMContext):
+    data = state.get_data()
+    location = data.get('ans')
+    await location
 
 
 # чтобы установить новые значения для переменной location
 async def set_location(message: types.Message):
-    global location
+    await message.answer('Укажите новую локацию')
+    await Location.location.set()
+'''
+   global location
     location = message.text
     msg = f"Установлена новая локация: {location}"
     await message.answer(msg, reply_markup=inline.WEATHER)
+'''
+
+
+async def answer_location(message: types.Message, state: FSMContext):
+    answer = message.text
+    await state.update_data(ans=answer)
+    await message.answer(f"Новое местоположение: {answer}", reply_markup=inline.WEATHER)
 
 
 def register_location(dp: Dispatcher):
-    dp.register_message_handler(set_location, content_types=types.ContentTypes.TEXT, state="*")
+    dp.register_message_handler(set_location, commands=["set_location"], state="*")
+    dp.register_message_handler(answer_location, content_types=types.ContentTypes.TEXT, state=Location.location)
--- tgbot/handlers/weather_commands.py
+++ tgbot/handlers/weather_commands.py
@@ -3,42 +3,43 @@ from aiogram import types, Dispatcher
 import tgbot.handlers.weather as wt
 from tgbot.keyboards import inline
 from tgbot.handlers.coordinates import get_location
+from aiogram.dispatcher.storage import FSMContext
 
 
 # Commands
-async def cmd_temperature(message: types.Message):
-    location = get_location()
+async def cmd_temperature(message: types.Message, state: FSMContext):
+    location = get_location(state)
     msg = wt.get_temperature(location)
     await message.answer(msg, reply_markup=inline.TEMPERATURE)
 
 
-async def cmd_wind(message: types.Message):
-    location = get_location()
+async def cmd_wind(message: types.Message, state: FSMContext):
+    location = get_location(state)
     msg = wt.get_wind(location)
     await message.answer(msg, reply_markup=inline.WIND)
 
 
-async def cmd_suntime(message: types.Message):
-    location = get_location()
+async def cmd_suntime(message: types.Message, state: FSMContext):
+    location = get_location(state)
     msg = wt.get_suntime(location)
     await message.answer(msg, reply_markup=inline.SUNTIME)
 
 
 # Callbaks
-async def call_temperature(callback: types.CallbackQuery):
-    location = get_location()
+async def call_temperature(callback: types.CallbackQuery, state: FSMContext):
+    location = get_location(state)
     msg = wt.get_temperature(location)
     await callback.message.answer(msg, reply_markup=inline.TEMPERATURE)
 
 
-async def call_wind(callback: types.CallbackQuery):
-    location = get_location()
+async def call_wind(callback: types.CallbackQuery, state: FSMContext):
+    location = get_location(state)
     msg = wt.get_wind(location)
     await callback.message.answer(msg, reply_markup=inline.WIND)
 
 
-async def call_suntime(callback: types.CallbackQuery):
-    location = get_location()
+async def call_suntime(callback: types.CallbackQuery, state: FSMContext):
+    location = get_location(state)
     msg = wt.get_suntime(location)
     await callback.message.answer(msg, reply_markup=inline.SUNTIME)
 
